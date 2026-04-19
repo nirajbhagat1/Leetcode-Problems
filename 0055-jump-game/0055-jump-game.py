86f2be87f798863d54088dc3594e53dc07602323
Niraj Bhagat <147089356+nirajbhagat1@users.noreply.github.com>
2026-04-19 22:33:07 +0530

    Time: 27 ms (30.54%), Space: 20.5 MB (35.61%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,3 +1,14 @@
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         max_reach = 0
+        
+#         for i in range(len(nums)):
+#             if i > max_reach:
+#                 return False
+            
+#             max_reach = max(max_reach, i + nums[i])
+        
+#         return True
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         max_reach = 0
@@ -5,7 +16,6 @@ class Solution:
         for i in range(len(nums)):
             if i > max_reach:
                 return False
-            
             max_reach = max(max_reach, i + nums[i])
         
         return True
