6b30eaa4530c051df10498af8a4709ce326048a4
Niraj Bhagat <147089356+nirajbhagat1@users.noreply.github.com>
2026-04-19 22:47:34 +0530

    Time: 9 ms (94.6%), Space: 20.1 MB (98.04%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -9,13 +9,15 @@
 #             max_reach = max(max_reach, i + nums[i])
         
 #         return True
+# 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        max_reach = 0
-        
-        for i in range(len(nums)):
-            if i > max_reach:
+        gas = 0
+        for n in nums:
+            if gas < 0:
                 return False
-            max_reach = max(max_reach, i + nums[i])
-        
+            elif n > gas:
+                gas = n
+            gas -= 1
+            
         return True
