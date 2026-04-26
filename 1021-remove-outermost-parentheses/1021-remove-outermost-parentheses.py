a6c95aebe425082cfaf551bbfb7033fae0fb3ec9
Niraj Bhagat <147089356+nirajbhagat1@users.noreply.github.com>
2026-04-26 18:39:09 +0530

    Time: 0 ms (100%), Space: 19.5 MB (10.93%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -1,35 +1,46 @@
-class Solution:
-    def removeOuterParentheses(self, s: str) -> str:
-        stack=[]
-        removeStartIndex=[]
-        removeEndIndex=[]
+# class Solution:
+#     def removeOuterParentheses(self, s: str) -> str:
+#         stack=[]
+#         removeStartIndex=[]
+#         removeEndIndex=[]
 
-        for i in range(len(s)):
-            if s[i]=="(":
-                stack.append('(')
-                if(len(stack)==1):
-                    removeStartIndex.append(i)
+#         for i in range(len(s)):
+#             if s[i]=="(":
+#                 stack.append('(')
+#                 if(len(stack)==1):
+#                     removeStartIndex.append(i)
             
-            else:
-                stack.pop()
-                if(len(stack)==0):
-                     removeEndIndex.append(i)
+#             else:
+#                 stack.pop()
+#                 if(len(stack)==0):
+#                      removeEndIndex.append(i)
     
        
-        lst = list(s)
+#         lst = list(s)
 
 
 
-        for index in removeStartIndex:
-            lst[index]=""
-        for index in removeEndIndex:
-            lst[index]=""
+#         for index in removeStartIndex:
+#             lst[index]=""
+#         for index in removeEndIndex:
+#             lst[index]=""
 
-        s = "".join(lst)
+#         s = "".join(lst)
 
-        return s
+#         return s
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        res = []
+        depth = 0
 
+        for ch in s:
+            if ch == '(':
+                if depth > 0:
+                    res.append(ch)
+                depth += 1
+            else:
+                depth -= 1
+                if depth > 0:
+                    res.append(ch)
 
-
-
-        
+        return "".join(res)
