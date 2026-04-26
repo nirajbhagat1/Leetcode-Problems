b3085f4d55f3ee8f04f2904209d5579f3c88e2d8
Niraj Bhagat <147089356+nirajbhagat1@users.noreply.github.com>
2026-04-26 22:02:22 +0530

    Time: 6 ms (65.72%), Space: 20.3 MB (32.74%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
@@ -1,11 +1,6 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
-
-    
-        for i in range(len(num)-1,-1,-1):
-          
-            if(int(num[i])%2!=0):
-                return num[0:i+1]
-
-      
+        for i in range(len(num) - 1, -1, -1):
+            if num[i] in "13579":   # no int conversion
+                return num[:i+1]
         return ""
