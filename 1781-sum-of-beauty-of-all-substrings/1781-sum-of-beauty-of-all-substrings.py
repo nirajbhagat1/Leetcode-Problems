2f30c2f61cb036d9a52d33c49f22ef1f8f1c142d
Niraj Bhagat <147089356+nirajbhagat1@users.noreply.github.com>
2026-04-29 00:13:24 +0530

    Time: 1421 ms (60.07%), Space: 19.3 MB (62.09%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
@@ -1,22 +1,39 @@
-from collections import Counter
-class Solution:
-    def beautySum(self, s: str) -> int:
-        total=0
+# from collections import Counter
+# class Solution:
+#     def beautySum(self, s: str) -> int:
+#         total=0
        
 
-        substrings = [
-            s[i:j]
-            for i in range(len(s))
-            for j in range(i+1, len(s)+1)
-            if len(set(s[i:j])) >= 2
-        ]
-        for subs in substrings:
-            total+=self.countBeauty(subs)
+#         substrings = [
+#             s[i:j]
+#             for i in range(len(s))
+#             for j in range(i+1, len(s)+1)
+#             if len(set(s[i:j])) >= 2
+#         ]
+#         for subs in substrings:
+#             total+=self.countBeauty(subs)
     
-        return total
+#         return total
 
         
-    def countBeauty(self,s:str)->int:
-        freq=Counter(s)
-        values=freq.values()
-        return max(values)-min(values)
+#     def countBeauty(self,s:str)->int:
+#         freq=Counter(s)
+#         values=freq.values()
+#         return max(values)-min(values)
+from collections import Counter
+
+class Solution:
+    def beautySum(self, s: str) -> int:
+        n = len(s)
+        total = 0
+
+        for i in range(n):
+            freq = Counter()
+            
+            for j in range(i, n):
+                freq[s[j]] += 1
+                
+                if len(freq) >= 2:
+                    total += max(freq.values()) - min(freq.values())
+
+        return total
