66014f4a1136307036ade00f48b49a429d3b0a80
Niraj Bhagat <147089356+nirajbhagat1@users.noreply.github.com>
2026-06-24 22:48:51 +0530

    Time: 12 ms (37.57%), Space: 22.8 MB (60.19%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -0,0 +1,55 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        if not lists:
+            return None
+
+        while len(lists) > 1:
+            merged = []
+
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if i + 1 < len(lists) else None
+
+                merged.append(self.mergeSort(l1, l2))
+
+            lists = merged
+
+        return lists[0]
+
+    def mergeSort(
+        self,
+        l1: Optional[ListNode],
+        l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+
+        dummy = ListNode(0)
+        curr = dummy
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+
+            curr = curr.next
+
+        if l1:
+            curr.next = l1
+        if l2:
+            curr.next = l2
+
+        return dummy.next
